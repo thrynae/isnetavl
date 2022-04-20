abbecc5ba0c841d73df14e633c89443f1e32b601
thrynae <64639801+thrynae@users.noreply.github.com>
2022-04-20 11:43:36 +0200

    bugfix
    An internal function was missing

--- isnetavl.m
+++ isnetavl.m
@@ -1,17 +1,26 @@
-function [connected,timing]=isnetavl(test_Matlab_instead_of_system)
-% Check for an internet connection by pinging Google.
-% Optional second output is the ping time (0 if not connected).
+function [connected,timing]=isnetavl(use_HTML_test_only)
+%Check for an internet connection by pinging Google
 %
-% Includes a fallback to HTML if usage of ping is not allowed. This increases the measured ping.
+% Syntax:
+%   [connected,timing]=isnetavl(use_HTML_test_only)
 %
-% If you provide an input, the HTML method will be used, which has the benefit of testing if Matlab
-% (or Octave) is able to connect to the internet. This might be relevant if there are proxy
-% settings or firewall rules specific to Matlab/Octave.
+% Input/output arguments:
+% connected:
+%   A logical denoting the connectivity. Note that this may return unexpected results if Matlab has
+%   separate proxy settings and/or if your DNS is having issues.
+% timing:
+%   This contains the ping time as a double and defaults to 0 if there is no connection.
+%   Note that this value will be larger than the true ping time if the HTML fallback is used.
+% use_HTML_test_only:
+%   If the input is convertible to true the HTML method will be used, which has the benefit of
+%   testing if Matlab (or Octave) is able to connect to the internet. This might be relevant if
+%   there are proxy settings or firewall rules specific to Matlab/Octave.
+%   Note that the ping value will be larger than the true value if the HTML fallback is used.
 %
 %/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%
 %|                                                                         |%
-%|  Version: 2.0.0                                                         |%
-%|  Date:    2021-05-19                                                    |%
+%|  Version: 2.0.1                                                         |%
+%|  Date:    2022-04-20                                                    |%
 %|  Author:  H.J. Wisselink                                                |%
 %|  Licence: CC by-nc-sa 4.0 ( creativecommons.org/licenses/by-nc-sa/4.0 ) |%
 %|  Email = 'h_j_wisselink*alumnus_utwente_nl';                            |%
@@ -26,8 +35,8 @@ function [connected,timing]=isnetavl(test_Matlab_instead_of_system)
 % - If you have proxy/firewall settings specific to Matlab/Octave, make sure to use the HTML method
 %   by providing an input.
 
-if nargin==0,                                                        tf=false;
-else,[tf1,tf2]=test_if_scalar_logical(test_Matlab_instead_of_system);tf=tf1&&tf2;
+if nargin==0,                                             tf=false;
+else,[tf1,tf2]=test_if_scalar_logical(use_HTML_test_only);tf=tf1&&tf2;
 end
 if tf
     %(the timing is not reliable)
@@ -51,6 +60,130 @@ else
     end
 end
 end
+function tf=ifversion(test,Rxxxxab,Oct_flag,Oct_test,Oct_ver)
+%Determine if the current version satisfies a version restriction
+%
+% To keep the function fast, no input checking is done. This function returns a NaN if a release
+% name is used that is not in the dictionary.
+%
+% Syntax:
+%   tf=ifversion(test,Rxxxxab)
+%   tf=ifversion(test,Rxxxxab,'Octave',test_for_Octave,v_Octave)
+%
+% Input/output arguments:
+% tf:
+%   If the current version satisfies the test this returns true. This works similar to verLessThan.
+% Rxxxxab:
+%   A char array containing a release description (e.g. 'R13', 'R14SP2' or 'R2019a') or the numeric
+%   version (e.g. 6.5, 7, or 9.6).
+% test:
+%   A char array containing a logical test. The interpretation of this is equivalent to
+%   eval([current test Rxxxxab]). For examples, see below.
+%
+% Examples:
+% ifversion('>=','R2009a') returns true when run on R2009a or later
+% ifversion('<','R2016a') returns true when run on R2015b or older
+% ifversion('==','R2018a') returns true only when run on R2018a
+% ifversion('==',9.9) returns true only when run on R2020b
+% ifversion('<',0,'Octave','>',0) returns true only on Octave
+% ifversion('<',0,'Octave','>=',6) returns true only on Octave 6 and higher
+%
+% The conversion is based on a manual list and therefore needs to be updated manually, so it might
+% not be complete. Although it should be possible to load the list from Wikipedia, this is not
+% implemented.
+%
+%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%
+%|                                                                         |%
+%|  Version: 1.1.1                                                         |%
+%|  Date:    2022-03-14                                                    |%
+%|  Author:  H.J. Wisselink                                                |%
+%|  Licence: CC by-nc-sa 4.0 ( creativecommons.org/licenses/by-nc-sa/4.0 ) |%
+%|  Email = 'h_j_wisselink*alumnus_utwente_nl';                            |%
+%|  Real_email = regexprep(Email,{'*','_'},{'@','.'})                      |%
+%|                                                                         |%
+%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%
+%
+% Tested on several versions of Matlab (ML 6.5 and onward) and Octave (4.4.1 and onward), and on
+% multiple operating systems (Windows/Ubuntu/MacOS). For the full test matrix, see the HTML doc.
+% Compatibility considerations:
+% - This is expected to work on all releases.
+
+% The decimal of the version numbers are padded with a 0 to make sure v7.10 is larger than v7.9.
+% This does mean that any numeric version input needs to be adapted. multiply by 100 and round to
+% remove the potential for float rounding errors.
+% Store in persistent for fast recall (don't use getpref, as that is slower than generating the
+% variables and makes updating this function harder).
+persistent  v_num v_dict octave
+if isempty(v_num)
+    % Test if Octave is used instead of Matlab.
+    octave=exist('OCTAVE_VERSION', 'builtin');
+    
+    % Get current version number. This code was suggested by Jan on this thread:
+    % https://mathworks.com/matlabcentral/answers/1671199#comment_2040389
+    v_num = [100, 1] * sscanf(version, '%d.%d', 2);
+    
+    % Get dictionary to use for ismember.
+    v_dict={...
+        'R13' 605;'R13SP1' 605;'R13SP2' 605;'R14' 700;'R14SP1' 700;'R14SP2' 700;
+        'R14SP3' 701;'R2006a' 702;'R2006b' 703;'R2007a' 704;'R2007b' 705;
+        'R2008a' 706;'R2008b' 707;'R2009a' 708;'R2009b' 709;'R2010a' 710;
+        'R2010b' 711;'R2011a' 712;'R2011b' 713;'R2012a' 714;'R2012b' 800;
+        'R2013a' 801;'R2013b' 802;'R2014a' 803;'R2014b' 804;'R2015a' 805;
+        'R2015b' 806;'R2016a' 900;'R2016b' 901;'R2017a' 902;'R2017b' 903;
+        'R2018a' 904;'R2018b' 905;'R2019a' 906;'R2019b' 907;'R2020a' 908;
+        'R2020b' 909;'R2021a' 910;'R2021b' 911;'R2022a' 912};
+end
+
+if octave
+    if nargin==2
+        warning('HJW:ifversion:NoOctaveTest',...
+            ['No version test for Octave was provided.',char(10),...
+            'This function might return an unexpected outcome.']) %#ok<CHARTEN>
+        if isnumeric(Rxxxxab)
+            v=0.1*Rxxxxab+0.9*fix(Rxxxxab);v=round(100*v);
+        else
+            L=ismember(v_dict(:,1),Rxxxxab);
+            if sum(L)~=1
+                warning('HJW:ifversion:NotInDict',...
+                    'The requested version is not in the hard-coded list.')
+                tf=NaN;return
+            else
+                v=v_dict{L,2};
+            end
+        end
+    elseif nargin==4
+        % Undocumented shorthand syntax: skip the 'Octave' argument.
+        [test,v]=deal(Oct_flag,Oct_test);
+        % Convert 4.1 to 401.
+        v=0.1*v+0.9*fix(v);v=round(100*v);
+    else
+        [test,v]=deal(Oct_test,Oct_ver);
+        % Convert 4.1 to 401.
+        v=0.1*v+0.9*fix(v);v=round(100*v);
+    end
+else
+    % Convert R notation to numeric and convert 9.1 to 901.
+    if isnumeric(Rxxxxab)
+        v=0.1*Rxxxxab+0.9*fix(Rxxxxab);v=round(100*v);
+    else
+        L=ismember(v_dict(:,1),Rxxxxab);
+        if sum(L)~=1
+            warning('HJW:ifversion:NotInDict',...
+                'The requested version is not in the hard-coded list.')
+            tf=NaN;return
+        else
+            v=v_dict{L,2};
+        end
+    end
+end
+switch test
+    case '==', tf= v_num == v;
+    case '<' , tf= v_num <  v;
+    case '<=', tf= v_num <= v;
+    case '>' , tf= v_num >  v;
+    case '>=', tf= v_num >= v;
+end
+end
 function [connected,timing]=isnetavl___ping_via_html
 %Ping is blocked by some organizations. As an alternative, the google.com page can be loaded as a
 %normal HTML, which should work as well, although it is slower. This also means the ping timing is
@@ -58,7 +191,7 @@ function [connected,timing]=isnetavl___ping_via_html
 persistent UseWebread
 if isempty(UseWebread)
     try no_webread=isempty(which(func2str(@webread)));catch,no_webread=true;end
-	UseWebread=~no_webread;
+    UseWebread=~no_webread;
 end
 try
     then=now;
@@ -151,4 +284,47 @@ end
 %Both methods failed, internet is down. Leave the value of tf (and the persistent variable) set to
 %empty so it is tried next time.
 tf=[];
+end
+function [isLogical,val]=test_if_scalar_logical(val)
+%Test if the input is a scalar logical or convertible to it.
+%The char and string test are not case sensitive.
+%(use the first output to trigger an input error, use the second as the parsed input)
+%
+% Allowed values:
+%- true or false
+%- 1 or 0
+%- 'on' or 'off'
+%- "on" or "off"
+%- matlab.lang.OnOffSwitchState.on or matlab.lang.OnOffSwitchState.off
+%- 'enable' or 'disable'
+%- 'enabled' or 'disabled'
+persistent states
+if isempty(states)
+    states={true,false;...
+        1,0;...
+        'on','off';...
+        'enable','disable';...
+        'enabled','disabled'};
+    if ifversion('>=','R2016b','<',0)
+        states(end+1,:)={string('on'),string('off')}; %#ok<STRQUOT>
+    end
+end
+isLogical=true;
+try
+    if isa(val,'char') || isa(val,'string')
+        try val=lower(val);catch,end
+    end
+    for n=1:size(states,1)
+        for m=1:2
+            if isequal(val,states{n,m})
+                val=states{1,m};return
+            end
+        end
+    end
+    if isa(val,'matlab.lang.OnOffSwitchState')
+        val=logical(val);return
+    end
+catch
+end
+isLogical=false;
 end
